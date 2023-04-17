9ee8549b073e182db38015a7feaee11fbeb40495
Lucas Rossi <99259327+LucasRossi3@users.noreply.github.com>
2023-04-17 13:13:06 -0300

    Add files via upload

--- Atividade9/Atividade9.js
+++ Atividade9/Atividade9.js
@@ -1,27 +1,25 @@
-
 numMat = 3;
-var numero = new Array(3);
+var numero = new Array(numMat);
 
-for(var i = 0; i < numMat; i++)
-    numero[i] = parseInt(prompt("Digite o número " + (i + 1)));
+numero[0] = parseInt(prompt("Digite o número " + "1"));
+numero[1] = parseInt(prompt("Digite o número " + "2"));
+numero[2] = parseInt(prompt("Digite o número " + "3"));
 
-function maiorNum(numero)
-{
-    var max = 0;
+function maiorNum(numero) {
+  var max = 0;
 
-    for(i = 0; i < numMat; i++)
-    {
-        if(numero[i] > max)
-            max = numero[i];
-    }
+  for (i = 0; i < numMat; i++) {
+    if (numero[i] > max) max = numero[i];
+  }
 
-    return max;
+  return max;
 }
 
-function ordenarCresc(numero)
-{
-    n = n.sort(function(a, b){ return a - b; } );
-    return numero;
+function ordenarCresc(numero) {
+  numero = numero.sort(function (a, b) {
+    return a - b;
+  });
+  return numero;
 }
 
 document.write("O maior número é: " + maiorNum(numero) + "<br>");
